fcf9446c1903f463cbe281bda50697f9153a9ab7
ALfredMarcel <1412556777@qq.com>
2022-11-05 20:11:25 +0800

    fix scanoss check error

--- examples/NS/src/utils.py
+++ examples/NS/src/utils.py
@@ -18,41 +18,24 @@ import numpy as np
 from scipy.io import loadmat
 
 
-# Load training data
 def load_training_data(num):
     data = loadmat("data/cylinder_nektar_wake.mat")
-    u_star = data["U_star"]  # N x 2 x T
-    p_star = data["p_star"]  # N x T
-    t_star = data["t"]  # T x 1
-    x_star = data["X_star"]  # N x 2
-    n = x_star.shape[0]
-    t = t_star.shape[0]
-    # Rearrange Data
-    xx = np.tile(x_star[:, 0:1], (1, t))  # N x T
-    yy = np.tile(x_star[:, 1:2], (1, t))  # N x T
-    tt = np.tile(t_star, (1, n)).T  # N x T
-    uu = u_star[:, 0, :]  # N x T
-    vv = u_star[:, 1, :]  # N x T
-    pp = p_star  # N x T
-    x = xx.flatten()[:, None]  # NT x 1
-    y = yy.flatten()[:, None]  # NT x 1
-    tt = tt.flatten()[:, None]  # NT x 1
-    u = uu.flatten()[:, None]  # NT x 1
-    v = vv.flatten()[:, None]  # NT x 1
-    p = pp.flatten()[:, None]  # NT x 1
-    # training domain: X × Y = [1, 8] × [−2, 2] and T = [0, 7]
-    data1 = np.concatenate([x, y, tt, u, v, p], 1)
+    u_s, p_s, t_s, x_s = data["U_star"], data["p_star"], data["t"], data["X_star"]
+    n, t = x_s.shape[0], t_s.shape[0]
+    xx, yy = np.tile(x_s[:, 0:1], (1, t)), np.tile(x_s[:, 1:2], (1, t))
+    tt = np.tile(t_s, (1, n)).T
+    uu, vv = u_s[:, 0, :], u_s[:, 1, :]
+    pp = p_s
+
+    tmp = [xx.flatten()[:, None], yy.flatten()[:, None], tt.flatten()[:, None], uu.flatten()[:, None],
+           vv.flatten()[:, None], pp.flatten()[:, None]]
+    data1 = np.concatenate(tmp, 1)
     data2 = data1[:, :][data1[:, 2] <= 7]
     data3 = data2[:, :][data2[:, 0] >= 1]
     data4 = data3[:, :][data3[:, 0] <= 8]
     data5 = data4[:, :][data4[:, 1] >= -2]
     data_domain = data5[:, :][data5[:, 1] <= 2]
-    # choose number of training points: num =7000
+
     idx = np.random.choice(data_domain.shape[0], num, replace=False)
-    x_train = data_domain[idx, 0:1]
-    y_train = data_domain[idx, 1:2]
-    t_train = data_domain[idx, 2:3]
-    u_train = data_domain[idx, 3:4]
-    v_train = data_domain[idx, 4:5]
-    p_train = data_domain[idx, 5:6]
-    return [x_train, y_train, t_train, u_train, v_train, p_train]
+    return [data_domain[idx, 0:1], data_domain[idx, 1:2], data_domain[idx, 2:3], data_domain[idx, 3:4],
+            data_domain[idx, 4:5], data_domain[idx, 5:6]]
